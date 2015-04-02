0fcd18b069a23ea075c09e322c0d37e85b1392c9
Xuan Zhu <xzhu.pku@gmail.com>
2015-04-02 10:10:27 -0400

    add function to convert f to c

--- temp_covert.py
+++ temp_covert.py
@@ -1,5 +1,10 @@
-def f_to_k (temp):
+def f_to_k(temp):
 	return ((temp - 32) * (5.0/9.0) +273.15)
 
 def k_to_c(temp):
 	return (temp - 273.15)
+
+def f_to_c(temp):
+	temp_k = f_to_k(temp)
+	result = k_to_c(temp_k)
+	return(result)
